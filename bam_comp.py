9ed1a3ba73e0a416ab356d293e6785a108bfee8e
Max <101606897+peepoShy@users.noreply.github.com>
2022-04-28 21:03:18 +0300

    Add files via upload

---
+++ bam_comp.py
@@ -0,0 +1,119 @@
+import pysam
+import pandas as pd
+
+class BamComp:
+    def __init__(self, input_data=[], output_path='', is_single_ended=0):
+        if len(output_path) > 0 and output_path[-4:] != '.csv' and '\\' != output_path[-1] != '/': output_path += '/'
+        self.input_data = input_data #[{'label': 'Label1', 'path': '/path/input_1.bam', 'tool': 'tool_name'}, ...]
+        self.output_path = output_path if output_path[-4:] == '.csv' else output_path + 'BamComp_res.csv' #'/path/output.csv'
+        self.is_single_ended = is_single_ended
+        self.comp_data = {}
+
+    # Append more files to existing class object
+    def append_data(self, label, path, tool):
+        self.input_data.append({'label':label, 'path':path, 'tool':tool})
+
+    # Save result. If multiple_tables set to 1, multiple files will be created (one for each label)
+    def save(self, multiple_tables = 0, output_path = ''):
+        if len(output_path) > 0 and output_path[-4:] != '.csv' and '\\' != output_path[-1] != '/': output_path += '/'
+        if len(output_path) > 0: self.output_path = output_path if output_path[-4:] == '.csv' else output_path + 'BamComp_res.csv'
+        if not multiple_tables:
+            self.comp_data.to_csv(path_or_buf=self.output_path)
+            print('Saved as csv.', self.output_path)
+        else:
+            all_columns = self.comp_data.columns
+            output_paths = []
+            for file in self.input_data:
+                output_paths.append(self.output_path[:-4] +  '_' + file['label'] + self.output_path[-4:])
+                file_columns = [column for column in all_columns if column[:len(file['label'])] == file['label']]
+                self.comp_data.to_csv(path_or_buf=output_paths[-1], columns=file_columns)
+            print('Saved as csv. Path:\n', '\n'.join(output_paths), sep='')
+
+    def compare(self):
+        def get_edit_dist(cigar, reads_len):
+            match = 0
+            for el in cigar:
+                if el[0] == 0: match+=el[1]
+            return reads_len - match
+        
+        def find_tag(tags, target):
+            for tag in tags:
+                if tag[0] == target: return tag[-1]
+            return None
+        
+        # Returns only primary aligned reads and checking for multimapping.
+        def get_primary(reads, tool):
+            primary_reads, mm = [None, None], [None, None] # for reads without a pair will be set to None
+            for read in reads:
+                if bin(read.flag)[-12:-11] == '1': continue # skipping supplementary aligned reads (0x800)
+                place = 1 if bin(read.flag)[-8:-7] == '1' else 0 # position of read in pair (0x80)
+                if tool[:3] == 'bwa' or tool == 'ngm':
+                    primary_reads[place] = read
+                    # In order to check if reads are multimapped looking for specific flags (BWA and NGM tools)
+                    if (tool[:3] == 'bwa' and None != find_tag(read.tags, 'XA')) or (tool == 'ngm' and None != find_tag(read.tags, 'X0') > 0): 
+                        mm[place] = 1
+                    else: mm[place] = 0
+                else:
+                    if bin(read.flag)[-9:-8] == '1': mm[place] = 1 # Checking if read have secondary alignment (0x100)
+                    else: primary_reads[place] = read
+            for i, read in enumerate(primary_reads):
+                if read != None and mm[i] == None: mm[i] = 0
+            return primary_reads, mm
+
+        for file in self.input_data:
+            label, tool_name = file['label'], file['tool']
+            if file['path'][-4:] == '.bam': qualifier = 'rb' # for .BAM format files
+            elif file['path'][-4:] == '.sam': qualifier = 'r' # for .SAM format files
+            
+            with pysam.AlignmentFile(file['path'], qualifier) as f:
+                next_line = next(f)
+                for line in f: #reading file line by line 
+                    reads = [line] #list of reads
+                    if next_line.query_name != reads[0].query_name: reads[0], next_line = next_line, reads[0]
+                    else: 
+                        reads.append(next_line)
+                        while 1:
+                            try: next_line = next(f)
+                            except StopIteration: break #in case end of file is reached
+                            if next_line.query_name == reads[0].query_name : #looking for rows with same read ID
+                                reads.append(next_line)
+                            else: break
+                    
+                    index = reads[0].query_name 
+                    reads, mm = get_primary(reads, tool_name)
+                    if self.comp_data.get(index, None) == None: self.comp_data[index] = {}
+                    if self.is_single_ended:
+                        self.comp_data[index].update({
+                            label + '_flags': reads[0].flag,
+                            label + '_pos': reads[0].reference_start + 1, #indexing starts from 0
+                            label + '_chr': reads[0].reference_name,
+                            label + '_CIGAR': reads[0].cigarstring,
+                            label + '_edit_dist': get_edit_dist(reads[0].cigar, len(reads[0].query_sequence)),
+                            label + '_quality': reads[0].mapping_quality,
+                            label + '_MD': find_tag(reads[0].tags, 'MD'),
+                            label + '_multi': mm[0]
+                        })
+                    else:
+                        self.comp_data[index].update({
+                            label + '_flags': [reads[0].flag,reads[1].flag],
+                            label + '_pos': [reads[0].reference_start + 1, reads[1].reference_start + 1], #indexing starts from 0
+                            label + '_chr': [reads[0].reference_name, reads[1].reference_name],
+                            label + '_CIGAR': [reads[0].cigarstring, reads[1].cigarstring],
+                            label + '_edit_dist': [get_edit_dist(reads[0].cigar, len(reads[0].query_sequence)),\
+                                                   get_edit_dist(reads[1].cigar, len(reads[1].query_sequence))],
+                            label + '_quality': [reads[0].mapping_quality, reads[1].mapping_quality],
+                            label + '_MD': [find_tag(reads[0].tags, 'MD'), find_tag(reads[1].tags, 'MD')],
+                            label + '_multi': mm
+                        })
+        self.comp_data = pd.DataFrame.from_dict(self.comp_data).T
+
+from time import time
+st = time()
+test = BamComp([
+    {'label':'b2', 'path':'bowtie2_res.sam', 'tool':'b2'},
+    {'label':'ngm', 'path':'b_ngm_res.bam', 'tool':'ngm'}],
+    'changed_res.csv')
+test.compare()
+test.save()
+print(time() - st)
+
---
+++ test.py
@@ -0,0 +1,45 @@
+from bam_comp import BamComp
+
+# May need to install the pysam package for python: 
+#     python3 -m pip install pysam
+
+# Creating class object. 
+#     input_data requires LIST of DICTIONARIES with folliwing STRING fields: label, path, tool. (can be empty and field later through append_data function)
+#     output_path requires a STRING path to output directory or file
+# -------
+# Tool field in input_data can be empty ('tool':'') or contain tool tag (Should be field for ngm and bwa tools, because of specific tags). 
+# It should work with SAM and BAM files. 
+# Also should work with single end reads if "is_single_ended" set to 1 when creating class object (set to 0 by default).
+# -------
+test = BamComp(
+    input_data=[
+        {'label':'NGM_1', 'path':'~/GW/b_ngm_res.bam', 'tool':'ngm'},
+        {'label':'BWA_1', 'path':'~/GW/bwa_res.sam', 'tool':'bwa'}
+        ],
+    output_path='~/GW/output.csv',
+    is_single_ended=0
+    )
+
+# As mention before new files can be added with append_data function. Function requires STRING label, path and tool parameters
+test.append_data(label='BOWTIE2_1', path='~/GW/bowtie2_res.bam', tool='')
+
+# compare function will start parsing for each added file one by one
+test.compare()
+
+# After using compare function you can accsess created table by using *class_object_name*.comp_data
+# it will return PANDAS DataFrame with following columns (ID and 8 columns for each label):
+#   ID                  STRING          read index
+#   flags               LIST (INT)      SAM format flag for each read
+#   pos                 LIST (INT)      position of primary aligned reads in the pair
+#   chr                 LIST (STR)      chromosome
+#   CIGAR               LIST (STR)      cigar strings
+#   edit_dist           LIST (INT)      edit distance
+#   quality             LIST (INT)      quality of reads given by tool
+#   MD                  LIST (STR)      mismatch - MD tag (if available)
+#   multi               LIST (INT)      multimapping (0 - no multimapping / 1 - read multimapped)
+df = test.comp_data
+
+# Save result in output_path folder. If multiple_tables set to 1, multiple files will be created (one for each label). Parameters:
+#   multiple_tables     INTEGER     0 (default) or 1. If set to 0 - one large table with all labels will be created.
+#   output_path         STRING      optionall. Will replace output_path set when class object was created.
+test.save()
