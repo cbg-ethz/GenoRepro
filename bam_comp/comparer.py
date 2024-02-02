0991c5334c127408671bf291badecb7ee1ce0c2b
Mike <simonov.misha@gmail.com>
2024-02-02 21:44:10 +0300

    comparer results fix

--- bam_comp/comparer.py
+++ bam_comp/comparer.py
@@ -1,39 +1,53 @@
-#   --------------------------------------------------------------------------------
-#   FEATURE             |       NUMBER OF READS             |       PERCENTAGE     |
-#   --------------------------------------------------------------------------------
-#   Total_reads         | Initial number of reads           |     Always 100%      |
-#                       | (unfiltered, raw)                 |                      |
-#   --------------------------------------------------------------------------------
-#   Mapped_reads        | Number of reads without those     |                      |
-#                       | which were mapped neither with    |  from Total_reads    |
-#                       | original nor with replicated data |                      |
-#   --------------------------------------------------------------------------------
-#   Unambiguous_{label} | Number of unambiguous reads       |  from Mapped_reads   |
-#                       | by their label                    |                      |
-#   --------------------------------------------------------------------------------
-#   Common_unambiguous  | Number of common unambiguous reads|  from Mapped_reads   |
-#   --------------------------------------------------------------------------------
-#   Inconsistent_type1  | Unambiguous reads mapped only     |         from         |
-#                       | with original data                |  Common_unambiguous  |
-#   --------------------------------------------------------------------------------
-#   Inconsistent_type2  | Unambiguous reads mapped only     |         from         |
-#                       | with replicated data              |  Common_unambiguous  |
-#   --------------------------------------------------------------------------------
-#   Identical           | Identical reads in terms of       |         from         |
-#                       | position and edit distance        |  Common_unambiguous  |
-#   --------------------------------------------------------------------------------
-#   Consistent_global_  | Common unambiguous reads mapped   |         from         |
-#   _inconsistent_local | to the same position with         |  Common_unambiguous  |
-#                       | different edit distance           |                      |
-#   --------------------------------------------------------------------------------
-#   Inconsistent_global | Common unambiguous reads mapped   |         from         |
-#                       | to different positions            |  Common_unambiguous  |
-#                       | with different edit distance      |                      |
-#   --------------------------------------------------------------------------------
-#   Multi_mapped        | Common unambiguous reads mapped   |         from         |
-#                       | to different positions            |  Common_unambiguous  |
-#                       | with the same edit distance       |                      |
-#   --------------------------------------------------------------------------------
+#   ----------------------------------------------------------------------------------
+#   FEATURE             |       NUMBER OF READS             |        PERCENTAGE      |
+#   ----------------------------------------------------------------------------------
+#   Total_FASTQ_reads   | Initial number of reads           |      Always 100%       |
+#                       | within FASTQ File                 |                        |
+#   ----------------------------------------------------------------------------------
+#   Total_BAM_reads     | Number of alignments              | from Total_FASTQ_reads |
+#                       | within BAM File                   |                        |
+#   ----------------------------------------------------------------------------------
+#   Mapped_reads        | Number of reads without those     |                        |
+#                       | which were mapped neither with    | from Total_FASTQ_reads |
+#                       | original nor with replicated data |                        |
+#   ----------------------------------------------------------------------------------
+#   Mapped_reads_type1  | Number of reads mapped with       |                        |
+#                       | original data                     | from Total_FASTQ_reads |
+#                       |                                   |                        |
+#   ----------------------------------------------------------------------------------
+#   Mapped_reads_type2  | Number of reads mapped with       |                        |
+#                       | replicated data                   | from Total_FASTQ_reads |
+#                       |                                   |                        |
+#   ----------------------------------------------------------------------------------
+#   Unambiguous_type1   | Number of unambiguous reads       |   from Mapped_reads    |
+#                       | from original data                |                        |
+#   ----------------------------------------------------------------------------------
+#   Unambiguous_type2   | Number of unambiguous reads       |   from Mapped_reads    |
+#                       | from replicated data              |                        |
+#   ----------------------------------------------------------------------------------
+#   Common_unambiguous  | Number of common unambiguous reads|   from Mapped_reads    |
+#   ----------------------------------------------------------------------------------
+#   Inconsistent_type1  | Unambiguous reads mapped only     |          from          |
+#                       | with original data                |   Common_unambiguous   |
+#   ----------------------------------------------------------------------------------
+#   Inconsistent_type2  | Unambiguous reads mapped only     |          from          |
+#                       | with replicated data              |   Common_unambiguous   |
+#   ----------------------------------------------------------------------------------
+#   Identical           | Identical reads in terms of       |          from          |
+#                       | position and edit distance        |   Common_unambiguous   |
+#   ----------------------------------------------------------------------------------
+#   Consistent_global_  | Common unambiguous reads mapped   |          from          |
+#   _inconsistent_local | to the same position with         |   Common_unambiguous   |
+#                       | different edit distance           |                        |
+#   ----------------------------------------------------------------------------------
+#   Inconsistent_global | Common unambiguous reads mapped   |          from          |
+#                       | to different positions            |   Common_unambiguous   |
+#                       | with different edit distance      |                        |
+#   ----------------------------------------------------------------------------------
+#   Multi_mapped        | Common unambiguous reads mapped   |          from          |
+#                       | to different positions            |   Common_unambiguous   |
+#                       | with the same edit distance       |                        |
+#   ----------------------------------------------------------------------------------
 
 
 import os
